1ceaa90946849eb131da20918e007cf3d3125bbc
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2025-11-24 02:41:43 +0530

    buttons make responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,15 @@ console.log("jsloaded!!")
         mobileMenu.classList.toggle('hidden');
     });
 
+ let categoryBtns = document.querySelectorAll('.category-btn');
+    
+    categoryBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            categoryBtns.forEach(b => {
+                b.classList.remove('active', 'bg-red-600');
+                b.classList.add('bg-[#0D2851]');
+            });
+            btn.classList.add('active', 'bg-red-600');
+            btn.classList.remove('bg-[#0D2851]');
+        });
+    });
