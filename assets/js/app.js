c8e67ca9e0c436a980b2990f19c8ba34ee9335a1
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2025-11-24 01:54:14 +0530

    add search bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ console.log("jsloaded!!")
  let menuButton = document.getElementById('menuButton');
  let mobileMenu = document.getElementById('mobileMenu');
 
-        menuButton.addEventListener('click', () => {
-            mobileMenu.classList.toggle('hidden');
-        });
+    menuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+
