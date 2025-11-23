53854d9ae8a530586ecd1e2de67f9dafbc71dab2
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2025-11-24 01:20:11 +0530

    Responsive added to navbar (toggle Hamburger button)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
 console.log("jsloaded!!")
+
+ let menuButton = document.getElementById('menuButton');
+ let mobileMenu = document.getElementById('mobileMenu');
+
+        menuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
